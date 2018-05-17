80ce352eade7af4de7fb04e3f3b7e25743a9549b
Guang.Gao <Guang.Gao@kaiostech.com>
2018-05-17 12:12:00 +0800

    sort

--- test/test.js
+++ test/test.js
@@ -8,8 +8,8 @@ test('main', t => {
 
 
 test('compare', t => {
-	var array = ['张小里','高','王','赵啊', '张啊', ' ', '奥'];
-	var arrayResult = [' ', '奥', '高','王','张啊', '张小里', '赵啊'];
+	var array = ['张小里','高','agd','hhd', 'xdsfd', 'xasadf', 'yaldjflja','vvv','王','赵啊', 'rsdf','张啊', ' ', '奥'];
+	var arrayResult = [ ' ','agd','奥','高','hhd','rsdf','vvv','王','xasadf','xdsfd','yaldjflja','张啊','张小里','赵啊'];
 
 	var result = sortLocale(array, 'zh-CN');  //= array.sort(compare);
 	for (var i = result.length - 1; i >= 0; i--) {
